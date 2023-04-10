528d07b6c8671a1ac5b6b18989bda3bf8eae13c2
mdful <mdful@DESKTOP-DI0DNS7>
2023-04-09 23:28:18 -0400

    Add project files.

---
+++ bubble sort/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare Variable
+            int a;
+            int[] storeValue;
+            int b;
+            int i;
+            int j;
+            int temp;
+            
+            {
+                    //capture array
+                    Console.WriteLine("Enter the number of values to sort.");
+                    b = int.Parse(Console.ReadLine());
+                    storeValue = new int[b];
+
+                    for (a = 0; a <= storeValue.Length - 1; a++)
+                    {
+                        Console.WriteLine("Enter next number to sort.");
+                        storeValue[a] = int.Parse(Console.ReadLine());
+                    }
+
+                    //Sort Array
+                    for (j = b - 1; j > 0; j--)
+                    {
+                        for (i = 0; i < j; i++)
+                        {
+                            if (storeValue[i] > storeValue[i + 1])
+                            {
+                                temp = storeValue[i + 1];
+                                storeValue[i + 1] = storeValue[i];
+                                storeValue[i] = temp;
+                            }
+                        }
+                    }
+                    //Output sorted values
+                    Console.WriteLine("Sorted:");
+                    foreach (int p in storeValue)
+                        Console.Write(p + " ");
+                   
+                    Console.Read();
+               
+            }
+                    } 
+    }
+}
